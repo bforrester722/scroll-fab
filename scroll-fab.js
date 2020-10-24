3241da75c910410ef0ada67ab72a91e4e1d5479d
bforrester722 <b.forrester@live.com>
2020-10-24 16:29:02 -0500

    initial push

---
+++ scroll-fab.js
@@ -0,0 +1,61 @@
+
+import {AppElement, html} from '@smyd/app-shared/app-element.js';
+import {listen}   from '@smyd/app-functions/utils.js';
+import htmlString from './scroll-fab.html';
+import '@smyd/app-shared/app-icons.js';
+
+
+class ScrollFab extends AppElement {
+  static get is() { return 'scroll-fab'; }
+
+  static get template() {
+    return html([htmlString]);
+  }
+
+
+  connectedCallback() {
+    super.connectedCallback();
+    
+    this.__startFabAnimationControl();
+  }
+
+
+  __startFabAnimationControl() {
+    let ticking  = false; // perf
+    const height = window.innerHeight;
+
+    const toggleFabClass = () => {
+      if (ticking) { return; }
+      ticking = true;
+      window.requestAnimationFrame(async () => {
+        if (window.scrollY > height) {
+          if (!this.$.fab.classList.contains('fab-entry')) {
+            await import('@polymer/paper-fab/paper-fab.js');
+            this.$.fab.classList.add('fab-entry');
+          }
+        }
+        else if (this.$.fab.classList.contains('fab-entry')) {
+          this.$.fab.classList.remove('fab-entry');
+        }
+        ticking = false;
+      });
+    };
+
+    listen(document, 'scroll', toggleFabClass);
+  }
+
+
+  async __scrollToTopFabClicked() {
+    try {
+      await this.clicked();
+      window.scrollTo({behavior: 'smooth', top: 0});
+    }
+    catch (error) {
+      if (error === 'click debounced') { return; }
+      console.error(error);
+    }
+  }  
+  
+}
+
+window.customElements.define(ScrollFab.is, ScrollFab);
